769b96f77b4d47450165b75c64378f14fb67fbce
0Z0SK0 <45397007+0Z0SK0@users.noreply.github.com>
2023-12-23 15:26:41 +0500

    Fix payload in unit-test

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,31 +2,37 @@ package tests
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/0z0sk0/intersvyaz-go-test/server"
 	trackhandler "github.com/0z0sk0/intersvyaz-go-test/track/http/delivery"
 	trackrepo "github.com/0z0sk0/intersvyaz-go-test/track/repository"
 	trackusecase "github.com/0z0sk0/intersvyaz-go-test/track/usecase"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert/v2"
-	"io"
+	"github.com/google/uuid"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	// google uuid
 )
 
 func SetUpRouter() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
-	gin.DefaultWriter = io.Discard
+	//gin.SetMode(gin.ReleaseMode)
+	//gin.DefaultWriter = io.Discard
 	router := gin.Default()
 
 	return router
 }
 
+type TrackReq struct {
+	UUID string `json:"id"`
+	Page string `json:"page"`
+}
+
 /*
 unit тестирование на создание метрики, ничего сложного
 */
-func TestCreateTrackPostReturnsStatusOk(t *testing.T) {
+func TestCreateTrack(t *testing.T) {
 	conn := server.LoadDB()
 
 	r := SetUpRouter()
@@ -35,7 +41,11 @@ func TestCreateTrackPostReturnsStatusOk(t *testing.T) {
 
 	trackhandler.RegisterEndpoints(r, trackUseCase)
 
-	var jsonValue = []byte(`{"id":"60b496c1", "page":"demo"}`)
+	id := uuid.New()
+	jsonValue, _ := json.Marshal(TrackReq{UUID: id.String(), Page: "testPage"})
+
+	var jsonDecoded, _ = json.MarshalIndent(TrackReq{UUID: id.String(), Page: "testPage"}, "", "")
+	log.Printf("%s", string(jsonDecoded))
 
 	req, _ := http.NewRequest("POST", "/track", bytes.NewBuffer(jsonValue))
 
@@ -45,16 +55,9 @@ func TestCreateTrackPostReturnsStatusOk(t *testing.T) {
 }
 
 /*
-type TrackReq struct {
-	id uuid
-	page string
-}
-*/
-
-/*
 Псевдо-высоконагруженное unit тестирование сходное тесту выше
 */
-func TestHighLoadCreateTrackPostReturnsStatusOk(t *testing.T) {
+func TestCreateMultipleTrack(t *testing.T) {
 	conn := server.LoadDB()
 
 	r := SetUpRouter()
@@ -63,20 +66,12 @@ func TestHighLoadCreateTrackPostReturnsStatusOk(t *testing.T) {
 
 	trackhandler.RegisterEndpoints(r, trackUseCase)
 
-	// Чтобы избежать кеширования, лучше заменить готовую строку на генерированную
-	var jsonValue = []byte(`{"id":"60b496c1", "page":"demo"}`)
-
-        /*
-	trackReq := TrackReq{
-            id: uuid.String(),
-	    page: "testPage",
-        }
-	var jsonValue = json.Marshal(trackReq)
-	*/
-
 	// Почему 70?
 	// Из расчёта 250000 rph == ~70 rps
 	for i := 0; i < 70; i++ {
+		id := uuid.New()
+		jsonValue, _ := json.Marshal(TrackReq{UUID: id.String(), Page: "testPage"})
+
 		req, _ := http.NewRequest("POST", "/track", bytes.NewBuffer(jsonValue))
 		w := httptest.NewRecorder()
 		r.ServeHTTP(w, req)
